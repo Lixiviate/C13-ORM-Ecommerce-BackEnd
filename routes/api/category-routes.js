c7da87d1e38186ba190a9e0603cdfd5e3a981632
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-08-07 17:48:08 -0600

    Create findAll and findOne for category/tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,34 @@ const { Category, Product } = require("../../models");
 router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((category) => res.json(category))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.post("/", (req, res) => {
