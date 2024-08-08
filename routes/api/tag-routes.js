e60cd0d71a6dd8587289ed628a07577207379038
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-08-07 22:35:08 -0600

    Updated post, put, and delete for tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,14 +38,45 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
+  /* req.body should look like this...
+    {
+      "tag_name": "Jazz"
+    }
+  */
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
